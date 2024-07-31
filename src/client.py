fc434acb89e0ec69c436197e01bf274dda8fde2c
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-07-31 04:08:34 +0300

    adding try and except to manage errors client.py

--- src/client.py
+++ src/client.py
@@ -3,16 +3,19 @@ import socket
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 #AF stands for address family so here we are dealing with ipv4
 #sock_stream stands for tcp
+try:
+  with s:
+    s.connect((socket.gethostname(),1234))
 
-with s:
-  s.connect((socket.gethostname(),1234))
-  
-  while True:
-    msg = input('client: ')
-    s.sendall(msg.encode())
-    data = s.recv(1024)
-    if not data:
-      print('connection closed by server')
-      break
-    print(f'server: {data.decode()}')
-      
+    while True:
+      msg = input('client: ')
+      s.sendall(msg.encode())
+      data = s.recv(1024)
+      if not data:
+        print('connection closed by server')
+        break
+      print(f'server: {data.decode()}')
+
+except:
+  print("Client is shuted down")
+
