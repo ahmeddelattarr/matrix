32ce66f1e8f9ee2404771aa3e5a4f069885c4727
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-08-13 04:03:39 +0300

    Update client code to handle line-based communication
    - Changed data reading from chunks to line-by-line using `readline()`.
    - Added initial message handling to receive and display server response before user input.
    - Improved message output formatting by stripping whitespace.

--- src/client.py
+++ src/client.py
@@ -2,24 +2,31 @@ import asyncio
 import ssl
 import os
 
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 cert_path = os.path.join(current_dir, 'ssl/cert.pem')
 
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 context.load_verify_locations(cert_path)
 
+
+
 async def client():
+
     reader, writer = await asyncio.open_connection('localhost', 1234, ssl=context)
     try:
+        data = await reader.readline()
+        print(f"Server sent: {data.decode().strip()}")
+
         while True:
             msg = input('send: ')
             writer.write(msg.encode())
             await writer.drain()
-            data = await reader.read(1024)
+            data = await reader.readline()
             if not data:
                 print('Connection closed by server')
                 break
-            print(f'server recieved : {data.decode()}')
+            print(f'server received : {data.decode().strip()}')
     except ConnectionError as e:
         print(f"Connection error occurred: {e}")
     finally:
