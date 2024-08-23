f98891388b7a0e873fd6a9126fd1de94b3076e5a
Ahmed elattar <ahmeddelattarr@outlook.com>
2024-08-23 16:12:52 +0300

    Update testing.py

--- src/testing.py
+++ src/testing.py
@@ -9,39 +9,39 @@ from server import generate_ssl_cert,generate_token,verify_token,SECRET_KEY
 
 
 
-def test_generate_ssl_cert_files_exist(mocker):
+#def test_generate_ssl_cert_files_exist(mocker):
     # Mock os.path.exists to return True for both cert and key paths
-    mocker.patch('os.path.exists',returns_value=True)
+   # mocker.patch('os.path.exists',returns_value=True)
 
    # Mock subprocess.run and os.remove
-    mock_run=mocker.patch('subprocess.run')
-    mock_remove=mocker.patch('os.remove')
+   # mock_run=mocker.patch('subprocess.run')
+  #  mock_remove=mocker.patch('os.remove')
 
-    cert_path = '/path/to/cert.pem'
-    key_path = '/path/to/key.pem'
-    config_path = '/path/to/config.cnf'
+   # cert_path = '/path/to/cert.pem'
+   # key_path = '/path/to/key.pem'
+   # config_path = '/path/to/config.cnf'
 
-    generate_ssl_cert(cert_path,key_path,config_path)
+    #generate_ssl_cert(cert_path,key_path,config_path)
 
-    mock_run.assert_not_called()
-    mock_remove.assert_not_called()
+   # mock_run.assert_not_called()
+   # mock_remove.assert_not_called()
 
-def test_generate_ssl_cert_files_not_exist(mocker):
-    mocker.patch('os.path.exists',side_effect= lambda path: False if path in ['/path/to/cert.pem','/path/to/key.pem'] else True)
+#def test_generate_ssl_cert_files_not_exist(mocker):
+ #   mocker.patch('os.path.exists',side_effect= lambda path: False if path in ['/path/to/cert.pem','/path/to/key.pem'] else True)
 
-    mock_run = mocker.patch('subprocess.run')
-    mock_remove = mocker.patch('os.remove')
+  #  mock_run = mocker.patch('subprocess.run')
+ #   mock_remove = mocker.patch('os.remove')
 
-    cert_path = '/path/to/cert.pem'
-    key_path = '/path/to/key.pem'
-    config_path = '/path/to/config.cnf'
+    #cert_path = '/path/to/cert.pem'
+   # key_path = '/path/to/key.pem'
+    #config_path = '/path/to/config.cnf'
 
-    generate_ssl_cert(cert_path, key_path, config_path)
+  #  generate_ssl_cert(cert_path, key_path, config_path)
 
-    assert mock_run.call_count==3
+  #  assert mock_run.call_count==3
 
-    mock_remove.assert_called_once_with('csr.pem')
-
+   # mock_remove.assert_called_once_with('csr.pem')
+#
 
 def test_subprocess_failure(mocker):
     # Mock os.path.exists to return False for cert and key paths, True otherwise
